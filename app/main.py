eb491482fe0a6fa0d80ea6c0ff08f6c87e5888b4
Henrik Kiepe <hkiepe@inkontor.com>
2021-08-28 19:25:21 +0200

    Created a first product put request body

--- app/main.py
+++ app/main.py
@@ -4,11 +4,13 @@ from pydantic import BaseModel
 from fastapi import FastAPI
 
 
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
+class Product(BaseModel):
+    name: Optional[str] = None
+    ean: Optional[str] = None
+    gross_weight: Optional[float] = None
+    parcels: Optional[list] = None
+    shipment_price: Optional[list] = None
+    weight_out_of_range: Optional[bool] = False
 
 
 app = FastAPI()
@@ -19,9 +21,17 @@ async def read_main():
     return {"message": "Hello World of FastAPI with Traefik powered by Inkontor"}
 
 
-@app.put("/items/{item_id}")
+""" @app.put("/items/{item_id}")
 async def create_item(item_id: int, item: Item, q: Optional[str] = None):
     result = {"item_id": item_id, **item.dict()}
     if q:
         result.update({"q": q})
+    return result """
+
+
+@app.put("/products/{product_id}")
+async def create_product(product_id: str, product: Product):
+    result = {"item_id": product_id, **product.dict()}
+    if result['gross_weight'] > 40:
+        result['weight_out_of_range'] = True
     return result
