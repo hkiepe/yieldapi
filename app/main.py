c1dfb8be7f173679ff5c932068a53d3807637252
Henrik Kiepe <hkiepe@inkontor.com>
2021-08-28 18:00:03 +0200

    Added a request boddy

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
+from typing import Optional
+from pydantic import BaseModel
+
 from fastapi import FastAPI
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_main():
+async def read_main():
     return {"message": "Hello World of FastAPI with Traefik powered by Inkontor"}
+
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: Optional[str] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
