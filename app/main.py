f0d3ffe63c476f62cfb88d37dd840eae5c4b722d
Henrik Kiepe <hkiepe@inkontor.com>
2021-08-29 21:45:11 +0200

    created new virtual environment "product_database"

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta
 from typing import Optional
-
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
