e1f251fc582273de3f1a16d1dbedc7c1f9e8b2a6
Henrik Kiepe <hkiepe@inkontor.com>
2021-08-29 17:54:27 +0200

    added some authentification, for now without password hashing

--- app/main.py
+++ app/main.py
@@ -1,37 +1,90 @@
 from typing import Optional
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 
-from fastapi import FastAPI
 
-
-class Product(BaseModel):
-    name: Optional[str] = None
-    ean: Optional[str] = None
-    gross_weight: Optional[float] = None
-    parcels: Optional[list] = None
-    shipment_price: Optional[list] = None
-    weight_out_of_range: Optional[bool] = False
-
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
 
 app = FastAPI()
 
 
-@app.get("/")
-async def read_main():
-    return {"message": "Hello World of FastAPI with Traefik powered by Inkontor"}
+def fake_hash_password(password: str):
+    return "fakehashed" + password
 
 
-""" @app.put("/items/{item_id}")
-async def create_item(item_id: int, item: Item, q: Optional[str] = None):
-    result = {"item_id": item_id, **item.dict()}
-    if q:
-        result.update({"q": q})
-    return result """
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-@app.put("/products/{product_id}")
-async def create_product(product_id: str, product: Product):
-    result = {"item_id": product_id, **product.dict()}
-    if result['gross_weight'] > 40:
-        result['weight_out_of_range'] = True
-    return result
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def fake_decode_token(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(fake_users_db, token)
+    return user
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    rentalpoint=form_data.scopes
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    return {"access_token": user.username, "token_type": "bearer", "rentalpoin": rentalpoint}
+
+
+@app.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
